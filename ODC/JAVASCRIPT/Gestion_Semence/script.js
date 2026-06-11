90cd1c7bc88242ed45c86111792b5c154652e738
mariemeNDG <ndeyemariemesarrndong06@gmail.com>
2026-06-11 07:42:38 +0000

    login signin et dashboard

---
+++ ODC/JAVASCRIPT/Gestion_Semence/script.js
@@ -0,0 +1,131 @@
+// 
+const paysans = [
+    {
+        id: 1,
+        nom: "Samba DIOP",
+        localite: "Saloum",
+        culture: "Riz",
+        Semence: "250",
+        fertilisant: "340",
+        date: "06/09/2026",
+    },
+      {
+         id: 2,
+        nom: "Aliou DIOP",
+        localite: "Bakel",
+        culture: "Riz",
+        Semence: "250",
+        fertilisant: "340",
+        date: "06/09/2026",
+    },
+      {
+         id: 3,
+        nom: "Joe DIOP",
+        localite: "Thies",
+        culture: "Riz",
+        Semence: "250",
+        fertilisant: "340",
+        date: "06/09/2026",
+    }
+]
+
+
+const tbody = document.querySelector("tbody");
+const ide = document.querySelector("#ide");
+const nom = document.querySelector("#nomm");
+const localite = document.querySelector("#localite");
+const culture = document.querySelector("#culture");
+const semence = document.querySelector("#semence");
+const fertilisant = document.querySelector("#fertilisant");
+const date = document.querySelector("#date");
+let dateD = new Date();
+let jour = dateD.getDate();
+let mois = dateD.getMonth() + 1; // attention +1
+let annee = dateD.getFullYear();
+let dateFormat = `${jour}/${mois}/${annee}`;
+
+function tabPaysan(tab){
+    tab.forEach(element => {
+    let tr = document.createElement("tr");    
+    tr.innerHTML += `   <td>${element.id}</td>
+                        <td>${element.nom}</td>
+                        <td>${element.localite}</td>
+                        <td>${element.culture}</td>
+                        <td>${element.Semence}</td>
+                        <td>${element.fertilisant}</td>
+                        <td>${dateFormat}</td>
+                        
+                        `
+    tbody.append(tr);                    
+    });
+}
+
+let sort = true;
+
+nom.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.nom.localeCompare(b.nom) :
+            b.nom.localeCompare(a.nom) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     })
+   
+localite.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.localite.localeCompare(b.localite) :
+            b.localite.localeCompare(a.localite) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     })   
+     
+culture.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.culture.localeCompare(b.culture) :
+            b.culture.localeCompare(a.culture) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     }) 
+     
+semence.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.semence.localeCompare(b.semence) :
+            b.semence.localeCompare(a.semence) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     }) 
+     
+fertilisant.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.fertilisant.localeCompare(b.fertilisant) :
+            b.fertilisant.localeCompare(a.fertilisant) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     }) 
+     
+date.addEventListener("click", ()=>{
+         paysans.sort((a,b)=>{
+            return sort ?
+            a.date.localeCompare(b.date) :
+            b.date.localeCompare(a.date) ;
+        })
+        sort = !sort;
+        tbody.innerHTML = "";
+        tabPaysan(paysans)
+     })       
+
+tabPaysan(paysans);
